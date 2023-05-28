bc454bded773a393e1215564c82773c32c383a98
Yojit Shinde <yojitshinde@gmail.com>
2023-05-28 20:25:11 +0530

    csv file reading feature and the updated resume added

--- script.py
+++ script.py
@@ -2,29 +2,33 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
+import csv
 
-print("Enter Receiver's Email:")
-receiver_mail = input()
-sender_mail = ''
-sender_password = ''
+with open('emails.csv') as file:
+    reader_obj = csv.DictReader(file)
+    for obj in reader_obj:
+        receiver_email = obj['email']
 
-html_file = open('email.html', 'r')
-mail_content = html_file.read()
-message = MIMEMultipart('alternative')
-message['From'] = sender_mail
-message['To'] = receiver_mail
-message['Subject'] = 'Software Developer Application'
-message.attach(MIMEText(mail_content, 'html'))
+        sender_mail = ''
+        sender_password = ''
 
-filename = "YojitShinde-Resume.pdf"
-resume = MIMEApplication(open(filename, 'rb').read())
-resume.add_header('Content-Disposition','attachment', filename = 'YojitShinde-Resume.pdf')
-message.attach(resume)
+        html_file = open('email.html', 'r')
+        mail_content = html_file.read()
+        message = MIMEMultipart('alternative')
+        message['From'] = sender_mail
+        message['To'] = receiver_email
+        message['Subject'] = 'Software Developer Application'
+        message.attach(MIMEText(mail_content, 'html'))
 
-session = smtplib.SMTP('smtp.gmail.com', 587)
-session.starttls()
-session.login(sender_mail, sender_password)
-text = message.as_string()
-session.sendmail(sender_mail, receiver_mail, text)
-session.quit()
-print('Mail Sent')
+        filename = "YojitShinde-Resume.pdf"
+        resume = MIMEApplication(open(filename, 'rb').read())
+        resume.add_header('Content-Disposition','attachment', filename = 'YojitShinde-Resume.pdf')
+        message.attach(resume)
+
+        session = smtplib.SMTP('smtp.gmail.com', 587)
+        session.starttls()
+        session.login(sender_mail, sender_password)
+        text = message.as_string()
+        session.sendmail(sender_mail, receiver_email, text)
+        session.quit()
+        print('Mail Sent')
